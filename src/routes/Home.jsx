78862644bfa801ffb7fa876152b2801d211513d2
Natalie Schubert <schubertn25@gmail.com>
2023-06-07 15:44:38 +0200

    :lipstick: Updated instructions for the study

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -24,8 +24,18 @@ export default function Home() {
       <div className="p-5 m-4 bg-light rounded-3">
         <h3>Herzlich Willkommen</h3>
         <p>
-          Hier ist die Startseite. Hier werden Sie eine Einleitung und weitere
-          Informationen zur Studie lesen.
+          Ich führe zur derzeit eine Studie im Rahmen meiner Master-Arbeit
+          durch. Dazu beschäftige ich mich mit der Eingabe von Geburtsdaten in
+          Online-Formularen. Die Studie nimmt in etwa ??? Minuten in Anspruch
+          und ich wäre Ihnen für Ihre Teilnahme sehr dankbar. Die Angabe von
+          persönlichen Daten ist dazu nicht nötig und die Ergebnisse der Studie
+          werden anonym gespeichert und können Ihnen später nicht zugeordnet
+          werden.
+        </p>
+        <p>
+          Durch das Klicken auf <q>Weiter zur Anleitung</q> bestätigen Sie Ihr
+          Einverständnis zur Teilnahme. Es folgt eine genauere Anleitung zur
+          Durchführung der Studie.
         </p>
         <Link to="/Instructions" className="btn btn-custom" role="button">
           Weiter zur Anleitung
--- src/routes/Instructions.jsx
+++ src/routes/Instructions.jsx
@@ -22,8 +22,26 @@ export default function Instructions() {
       <div className="p-5 m-4 bg-light rounded-3">
         <h3>Anleitung</h3>
         <p>
-          Hier folgt eine Anleitung zur Durchführung der Studie. Wenn Sie auf
-          <q>Beginnen</q> drücken, dann beginnt die Studie. Juhu!
+          Die Studie besteht insgesamt aus ??? Durchgängen. In jedem Durchgang
+          müssen Sie ein vorgegebenes Datum eingeben und anschließend durch
+          einen <q>Weiter</q>-Button Ihre Eingabe bestätigen. Danach beginnt
+          automatisch der nächste Durchgang. Dabei variieren in allen
+          Durchgängen das Datum, welches Sie eingeben müssen, und auch die Art
+          der Eingabe. Bitte versuchen Sie, das gegebene Datum korrekt und
+          möglichst schnell einzugeben.
+        </p>
+        <p>
+          Im Anschluss an die eigentlich Studie werden Sie außerdem nach Ihrer
+          persönlichen Einschätzung zu den verschiedenen Eingabemethoden
+          gefragt, die Sie auf einer Skala von 1 bis 5 bewerten können.
+        </p>
+        <p>
+          Bitte stellen Sie sicher, dass während der gesamten Studie eine
+          stabile Internetverbindung besteht und Sie die Studie nicht
+          zwischenzeitlich unterbrechen.
+        </p>
+        <p>
+          Wenn Sie auf <q>Beginnen</q> klicken, startet die eigentliche Studie.
         </p>
         <Link to="/StudyPartOne" className="btn btn-custom" role="button">
           Beginnen
